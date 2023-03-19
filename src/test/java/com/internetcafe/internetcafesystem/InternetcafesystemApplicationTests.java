e19a2e4fc2fd607a9d70952f35a9e324e7b9a79e
220825213 <220825213@tut4life.ac.za>
2023-03-19 10:42:42 +0200

    Project creation aand initialised

---
+++ src/test/java/com/internetcafe/internetcafesystem/InternetcafesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internetcafe.internetcafesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetcafesystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
